4175f360e6c24bd5e226e1979ac787cdc66db309
loushang <loushang@vipkid.com.cn>
2019-07-16 17:42:28 +0800

    add slick sample

--- SlickDB/src/main/scala/org/clay/AkkaTest/SlickTest.scala
+++ SlickDB/src/main/scala/org/clay/AkkaTest/SlickTest.scala
@@ -1,11 +1,10 @@
 package org.clay.AkkaTest
 
 import com.typesafe.config.ConfigFactory
-import slick.driver.MySQLDriver.api._
+import slick.jdbc.MySQLProfile.api._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
 import scala.util.{Failure, Success}
 
 // define table structure(we can define many tables bindings)
@@ -62,5 +61,54 @@ object SlickTest extends App {
 
 
   // query by condition
-  val res2 = Await.result(db.run(slick_table.filter(_.age > 25).result), Duration.Inf)
+  val res2 = Await.result(db.run(slick_table.filter(_.age > 20).result), Duration.Inf)
+  println(res2)
+
+  // add（only 1 record)
+  /*val user1 = UserInfo(6L, "scarllet", 19)
+  val res3 = Await.result(db.run(slick_table += user1), Duration.Inf)*/ // return the insert numbers: 1, so no need to return
+
+  // add(batch records)
+  /*val user1 = UserInfo(6L, "scarllet", 19)
+  val user2 = UserInfo(7L, "mary", 21)
+  val newArray = Seq[UserInfo](user1, user2)
+  val res3 = Await.result(db.run(slick_table ++= newArray), Duration.Inf)*/ // return the insert numbers: 2, so no need to return
+
+  // update
+  val new_user = UserInfo(3L, "tashaxing", 23)
+  val res4 = Await.result(db.run(slick_table.filter(_.id === new_user.id).update(new_user)), Duration.Inf) // return effected row numbers
+
+  // delete
+  val res5 = Await.result(db.run(slick_table.filter(_.name === "tony").delete), Duration.Inf)
+
+
+  // return main column after insert
+  val user = UserInfo(0, "ethan", 21)
+  val save_sql = (slick_table returning slick_table.map(_.id)) += user
+  val user_id = Await.result(db.run(save_sql), Duration.Inf) // return created id(新插入的值的id)
+  println(s"user_id: ${user_id}")
+
+
+  // ---- use sql
+
+  // query sql
+  val res6 = Await.result(db.run(sql"""select * from scala_model where name = 'peper'""".as[(Long, String, Int)]),Duration.Inf)
+  println(s"res6: ${res6}")
+
+  // insert sql
+  /*val id = 10L
+  val name = "wilson"
+  val age = 29
+  val res7 = Await.result(db.run(sqlu"""insert into scala_model values($id, $name, $age)"""),Duration.Inf)
+  println(s"res7: ${res7}")*/  //返回插入的数量
+
+  // update sql
+  val res8 = Await.result(db.run(sqlu"""update scala_model set name='lily' where id=4"""), Duration.Inf)
+  println(s"res8: ${res8}")  //更新的数量
+
+
+  // delete sql
+  val res9 = Await.result(db.run(sqlu"""delete from scala_model where name='ethan'"""), Duration.Inf)
+  println(s"res9: ${res9}")   //删除的数量
+
 }
