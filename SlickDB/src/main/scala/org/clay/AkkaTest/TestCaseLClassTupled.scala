81529478003d89c7b9362d4ae1c81d8cc9e2a1b9
loushang <loushang@vipkid.com.cn>
2019-07-19 20:15:10 +0800

    add tupled

--- SlickDB/src/main/scala/org/clay/AkkaTest/TestCaseLClassTupled.scala
+++ SlickDB/src/main/scala/org/clay/AkkaTest/TestCaseLClassTupled.scala
@@ -5,6 +5,37 @@ case class User(name:String,email:String,age:Int)
 object TestCaseLClassTupled extends App {
 
   val userLikeData = ("John","John@doe.com",33)
+  val userLikeData1 = ("John","John@doe.com",33)
+
+  //tupled
   val user = User.tupled(userLikeData)
-  println(user)
+  val user1 = User.tupled(userLikeData1)
+
+  //copy
+  val user3 = user.copy(age = 11)
+  println(user3)
+
+  //unapply
+  //自动生成 unapply  用于模式匹配
+  def matchPrint(x: AnyRef) = {
+    x match {
+      case User(name,email,age) => println(s"match name: ${name}")
+      case _ => println("It's not User")
+    }
+  }
+  matchPrint(user3)
+
+  //解构
+  val User(name,_,_) = user
+  println(s"name-> ${name}")
+
+  //解构出 tuple
+  val transform: User =>Option[(String,String,Int)] = {
+    User.unapply _
+  }
+  val userTuple = transform(user)
+
+  println(s"userTuple: ${userTuple}")
+
+  println(user == user1)  //true
 }
